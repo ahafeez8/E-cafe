57642b4316e93e3a17947c79074afa3ee14da1d0
ahafeez8 <azkahafeez997@gmail.com>
2017-09-28 14:12:06 +0500

    Initial commit

---
+++ src/main/java/lab2/ecafe.java
@@ -0,0 +1,11 @@
+package lab2;
+
+public class ecafe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the E-Cafe\n");
+		
+	}
+
+}
